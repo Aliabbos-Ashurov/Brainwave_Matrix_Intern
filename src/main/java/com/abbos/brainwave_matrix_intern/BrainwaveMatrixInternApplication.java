d02a33820ff8272b046886b8382fce88e1e16c74
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2025-01-14 09:34:43 +0500

    def

---
+++ src/main/java/com/abbos/brainwave_matrix_intern/BrainwaveMatrixInternApplication.java
@@ -0,0 +1,13 @@
+package com.abbos.brainwave_matrix_intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainwaveMatrixInternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainwaveMatrixInternApplication.class, args);
+    }
+
+}
