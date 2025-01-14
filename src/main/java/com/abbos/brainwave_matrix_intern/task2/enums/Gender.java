ec8e7aefacdccbb7d39a43776cef0a370f462a13
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2025-01-14 15:04:11 +0500

    entity classes have been created

---
+++ src/main/java/com/abbos/brainwave_matrix_intern/task2/enums/Gender.java
@@ -0,0 +1,10 @@
+package com.abbos.brainwave_matrix_intern.task2.enums;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 14/January/2025  11:08
+ **/
+public enum Gender {
+    MALE,
+    FEMALE;
+}
---
+++ src/main/java/com/abbos/brainwave_matrix_intern/task2/enums/TransactionType.java
@@ -0,0 +1,11 @@
+package com.abbos.brainwave_matrix_intern.task2.enums;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 14/January/2025  10:59
+ **/
+public enum TransactionType {
+    PURCHASE,
+    CONSUMPTION,
+    NONE
+}
