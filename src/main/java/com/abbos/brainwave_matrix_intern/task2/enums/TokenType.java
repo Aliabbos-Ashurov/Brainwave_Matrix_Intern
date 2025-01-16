27c603e1f0bc96b296c8978ad638859cb2016327
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2025-01-16 11:45:17 +0500

    jwt token service & utils & dtos

---
+++ src/main/java/com/abbos/brainwave_matrix_intern/task2/enums/TokenType.java
@@ -0,0 +1,10 @@
+package com.abbos.brainwave_matrix_intern.task2.enums;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 16/January/2025  11:02
+ **/
+public enum TokenType {
+    BEARER,
+    BASIC
+}
